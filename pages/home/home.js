c58c1e3176aa9f5fdc8432719986f2abdfd74960
StephentTom <857329255@qq.com>
2019-09-02 22:59:45 +0800

    初识小程序

---
+++ pages/home/home.js
@@ -0,0 +1,117 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [
+      {
+        title: "请朋友喝咖啡",
+        section: [
+          {
+            avatar_url: "../../source/images/treat01.png",
+            desc: "给特别放入你"
+          },
+          {
+            avatar_url: "../../source/images/treat02.png",
+            desc: "我想对你说"
+          }
+        ]
+      },
+      {
+        title: "星礼卡表心意",
+        section: [
+          {
+            avatar_url: "../../source/images/card01.png",
+            desc: "来星巴克约会"
+          },
+          {
+            avatar_url: "../../source/images/card02.png",
+            desc: "爱要大声说出来"
+          },
+          {
+            avatar_url: "../../source/images/card03.png",
+            desc: "勇敢爱"
+          },
+          {
+            avatar_url: "../../source/images/card04.png",
+            desc: "一个人的精彩"
+          },
+          {
+            avatar_url: "../../source/images/card05.png",
+            desc: "萌萌哒"
+          },
+          {
+            avatar_url: "../../source/images/card06.png",
+            desc: "友谊万岁"
+          },
+          {
+            avatar_url: "../../source/images/card07.png",
+            desc: "恋人未满"
+          },
+          {
+            avatar_url: "../../source/images/card08.png",
+            desc: "生日快乐"
+          }
+        ]
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
