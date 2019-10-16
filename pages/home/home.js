e3e916cfcddc895744442ad11d9f7bdcfa0bec20
StephentTom <857329255@qq.com>
2019-10-16 23:48:19 +0800

    2019年10月16日23:48:17

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,50 +8,50 @@ Page({
     list: [
       {
         title: "请朋友喝咖啡",
-        section: [
+        sections: [
           {
-            avatar_url: "../../source/images/treat01.png",
+            avatar_url: "../../sources/images/treat01.png",
             desc: "给特别放入你"
           },
           {
-            avatar_url: "../../source/images/treat02.png",
+            avatar_url: "../../sources/images/treat02.png",
             desc: "我想对你说"
           }
         ]
       },
       {
         title: "星礼卡表心意",
-        section: [
+        sections: [
           {
-            avatar_url: "../../source/images/card01.png",
+            avatar_url: "../../sources/images/card01.jpg",
             desc: "来星巴克约会"
           },
           {
-            avatar_url: "../../source/images/card02.png",
+            avatar_url: "../../sources/images/card02.jpg",
             desc: "爱要大声说出来"
           },
           {
-            avatar_url: "../../source/images/card03.png",
+            avatar_url: "../../sources/images/card03.jpg",
             desc: "勇敢爱"
           },
           {
-            avatar_url: "../../source/images/card04.png",
+            avatar_url: "../../sources/images/card04.jpg",
             desc: "一个人的精彩"
           },
           {
-            avatar_url: "../../source/images/card05.png",
+            avatar_url: "../../sources/images/card05.jpg",
             desc: "萌萌哒"
           },
           {
-            avatar_url: "../../source/images/card06.png",
+            avatar_url: "../../sources/images/card06.jpg",
             desc: "友谊万岁"
           },
           {
-            avatar_url: "../../source/images/card07.png",
+            avatar_url: "../../sources/images/card07.jpg",
             desc: "恋人未满"
           },
           {
-            avatar_url: "../../source/images/card08.png",
+            avatar_url: "../../sources/images/card08.jpg",
             desc: "生日快乐"
           }
         ]
