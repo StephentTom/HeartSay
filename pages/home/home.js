86e3b62e4a2ac96a639773d5f6c1f70051649445
Mr Hong <857329255@qq.com>
2019-10-22 18:01:11 +0800

    2019年10月22日18:01:11

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-<<<<<<< HEAD
     list: [
       {
         title: "请朋友喝咖啡",
@@ -58,64 +57,13 @@ Page({
         ]
       }
     ]
-=======
-
->>>>>>> 2e4287392bc682f08f6836356df99602f1611fd9
   },
-
+  
   /**
-   * 生命周期函数--监听页面加载
+   * 跳转详情
    */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  toDetailPress: function(option) {
+    console.log(option)
   }
+
 })
